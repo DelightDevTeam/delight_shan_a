3256d9fb18f7b44e9952501eae51beef288cf939
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-08-16 09:49:36 +0630

    logo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -28,7 +28,7 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
+            <img class="animation__shake" src="{{asset('assets/img/dl_logo.png')}}" alt="AdminLTELogo" height="60" width="60">
         </div>
 
         <!-- Navbar -->
