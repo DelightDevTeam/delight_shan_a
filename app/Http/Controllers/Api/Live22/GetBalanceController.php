2e27772c231390a367380db11a43d060974c7fe2
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-08-23 12:09:27 +0630

    game login

--- app/Http/Controllers/Api/Live22/GetBalanceController.php
+++ app/Http/Controllers/Api/Live22/GetBalanceController.php
@@ -17,7 +17,7 @@ class GetBalanceController extends Controller
         $this->gameService = $gameService;
     }
 
-    public function getBalance(Request $request)
+     public function getBalance(Request $request)
     {
         // Ensure the user is authenticated and retrieve the current access token
         $token = Auth::user()->currentAccessToken()->token;
@@ -29,12 +29,16 @@ class GetBalanceController extends Controller
         // Pass the token to the GameService's getBalance method
         $response = $this->gameService->getBalance($token);
 
+        // Convert the JsonResponse to an array
+        $responseArray = $response->getData(true);
+
         // Assuming the API response contains the balance, but you want to replace it with your site's wallet balance
-        if ($response && isset($response['Balance'])) {
-            $response['Balance'] = Auth::user()->wallet->balance;
+        if (isset($responseArray['Balance'])) {
+            $responseArray['Balance'] = Auth::user()->wallet->balance;
         }
 
-        return response()->json($response);
+        // Return the modified response
+        return response()->json($responseArray);
     }
 
 }
