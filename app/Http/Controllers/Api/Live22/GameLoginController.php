1a0fe90864479884ca4ea84fb7749525f33afbb0
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-08-23 10:39:53 +0630

    game login

--- app/Http/Controllers/Api/Live22/GameLoginController.php
+++ app/Http/Controllers/Api/Live22/GameLoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Live22;
 use Illuminate\Http\Request;
 use App\Services\GameService;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class GameLoginController extends Controller
 {
@@ -17,21 +18,23 @@ class GameLoginController extends Controller
     }
 
     // Method to handle game login
-    public function Gamelogin(Request $request)
+    public function login(Request $request)
     {
         // Validate the incoming request
         $validated = $request->validate([
-            'player_id' => 'required|string',
             'game_code' => 'required|string',
             'launch_demo' => 'sometimes|boolean',
         ]);
 
+        // Get the authenticated user's ID
+        $playerId = Auth::user()->id;
+        
         // Call the gameLogin method from the GameService
         $response = $this->gameService->gameLogin(
-            $validated['player_id'],
+            $playerId,
             $validated['game_code'],
             $request->ip(), // Automatically get the client's IP address
-            $request->input('launch_demo', true) // Default to false if not provided
+            $request->input('launch_demo', false) // Default to false if not provided
         );
 
         // Return the response
