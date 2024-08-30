ad5e1dd4f1b963a455f8a4372b1729dbeb7433c3
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-08-30 08:41:40 +0630

    rollback update

--- app/Http/Controllers/Api/Live22/RollBackController.php
+++ app/Http/Controllers/Api/Live22/RollBackController.php
@@ -126,8 +126,10 @@ class RollBackController extends Controller
 
             return RollBackWebhookService::buildResponse(
                 StatusCode::OK,
-                $oldBalance,
-                $newBalance
+                //$oldBalance,
+                //$newBalance
+                number_format($oldBalance, 4, '.', ''),
+                number_format($newBalance + $request->getBetAmount(), 4, '.', '')
             );
         } catch (\Exception $e) {
             DB::rollBack();
