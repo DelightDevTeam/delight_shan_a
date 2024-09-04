7b1053e15613d05edee3d0a5a2be0e1541ba831d
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-09-04 16:26:11 +0630

    get balance fix

--- database/migrations/2024_07_31_025028_create_wallets_table.php
+++ database/migrations/2024_07_31_025028_create_wallets_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('wallets', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->decimal('balance', 15, 2)->default(0);
+            $table->decimal('balance', 15, 4)->default(0);
             $table->timestamps();
         });
     }
