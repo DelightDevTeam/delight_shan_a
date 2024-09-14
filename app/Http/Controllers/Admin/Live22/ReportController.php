bbf211e02d5595bf51a17b407c73ad5cf88a6815
AMK1215 <aungmyokhaing1215@gmail.com>
2024-09-14 14:25:35 +0630

    live 22 win lose report updated by amk

--- app/Http/Controllers/Admin/Live22/ReportController.php
+++ app/Http/Controllers/Admin/Live22/ReportController.php
@@ -43,7 +43,7 @@ class ReportController extends Controller
 }
 
 // for agent live 22 report
-public function AgentReport()
+    public function AgentReport()
 {
     // Get the authenticated agent's ID
     $agentId = Auth::user()->id;
@@ -63,13 +63,41 @@ public function AgentReport()
     )
     ->join('users', 'game_results.user_id', '=', 'users.id')
     // Filter results by the authenticated agent's ID
-    ->where('game_results.agent_id', $agentId)
+    ->where('users.agent_id', $agentId)
     ->groupBy('game_results.user_id', 'users.name')
     ->orderBy('id', 'desc')
     ->get();
 
     return view('admin.live_22.report.agent_report.index', compact('results'));
 }
+
+// public function AgentReport()
+// {
+//     // Get the authenticated agent's ID
+//     $agentId = Auth::user()->id;
+
+//     // Fetch the game results associated with the authenticated agent
+//     $results = GameResult::select(
+//         DB::raw('MAX(game_results.id) as id'),
+//         'game_results.user_id',
+//         'users.name as user_name',
+//         DB::raw('MAX(game_results.game_name) as game_name'),
+//         DB::raw('SUM(game_results.bet_amount) as total_bet_amount'),
+//         DB::raw('SUM(game_results.valid_bet_amount) as total_valid_bet_amount'),
+//         DB::raw('SUM(game_results.payout) as total_payout'),
+//         DB::raw('SUM(game_results.win_lose) as total_win_lose'),
+//         DB::raw('MAX(game_results.result_type) as result_type'),
+//         DB::raw('MAX(game_results.tran_date_time) as tran_date_time')
+//     )
+//     ->join('users', 'game_results.user_id', '=', 'users.id')
+//     // Filter results by the authenticated agent's ID
+//     ->where('game_results.agent_id', $agentId)
+//     ->groupBy('game_results.user_id', 'users.name')
+//     ->orderBy('id', 'desc')
+//     ->get();
+
+//     return view('admin.live_22.report.agent_report.index', compact('results'));
+// }
 
 
 }
